fcb3f85da894bf18c58bbe19031d31dc38de3cde
kriskata <kriskata5000@gmail.com>
2023-10-14 12:58:13 +0300

    Everything works with Gradle. Maven just slowed me down.

--- app_api/src/main/java/jcorechat/app_api/API.java
+++ app_api/src/main/java/jcorechat/app_api/API.java
@@ -1,12 +1,18 @@
 package jcorechat.app_api;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class API {
 
     public static void main(String[] args) {
 
-            System.out.println("App API");
 
+        System.out.println("App API");
 
+        SpringApplication app = new SpringApplication(API.class);
+        app.run(args);
     }
 }
